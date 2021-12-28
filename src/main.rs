f8ddc42845271f39598a218ee6fcec659e7cd415
Maxi Wittich <maxi.wittich@icloud.com>
2021-12-28 11:32:30 -0500

    Added interface for commmandline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use clap::{App, Arg, ArgMatches};
+
+macro_rules! println_verbose {
+    ($verbose:expr, $($arg:tt)*) => {
+        if $verbose {
+            println!($($arg)*);
+        }
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = get_app().get_matches();
+    let verbose = args.is_present("verbose");
+    let register = args.is_present("register");
+    println_verbose!(verbose, "Verbose is active");
+}
+
+fn get_app() -> App<'static, 'static>{
+    App::new("mtg analyizer")
+    .version("0.1")
+    .author("Maximilian Wittich <maxi.wittich@outlook.com>")
+    .about("Reads moxfield mtgo export lists to analyze the deck.")
+    .arg(
+        Arg::with_name("input")
+        .required(true)
+        .help("Path to decklist or folder")
+        .index(1)
+    )
+    .arg(
+        Arg::with_name("register")
+        .short("r")
+        .long("register")
+        .help("Takes a path to a folder containing decklists")
+    )
+    .arg(
+        Arg::with_name("verbose")
+        .short("v")
+        .long("verbose")
+        .help("If set, verbose mode is activated")
+    )
+
+
 }
